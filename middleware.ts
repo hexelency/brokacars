d6212c88e65bc6132d3c3b837e91054e2544b01f
hexelency <hexelency@gmail.com>
2025-05-26 15:38:08 +0100

    testing mode removed apim

--- middleware.ts
+++ middleware.ts
@@ -1,12 +1,9 @@
-// middleware.ts
 import { clerkMiddleware } from '@clerk/nextjs/server';
 
 export default clerkMiddleware();
 
 export const config = {
   matcher: [
-    '/dashboard(.*)', // protect dashboard area
-    '/history(.*)',   // other protected routes
-    // '/api/(.*)',      // protect API routes 
+    "/((?!_next|.*\\..*|favicon.ico).*)", // match all except static files
   ],
 };
