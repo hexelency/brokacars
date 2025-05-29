e60dc0f3c72a9ef63fb817a6f3f17957105b8100
hexelency <hexelency@gmail.com>
2025-05-29 03:14:32 +0100

    all layout shit corrected

--- app/(dashboard)/_components/booking/Booking.tsx
+++ app/(dashboard)/_components/booking/Booking.tsx
@@ -11,7 +11,7 @@ const Booking = () => {
       <span className='text-[10px] md:text-[1.29rem] font-semibold text-center p-1 block mt-1'>Book a ride
       </span>
 
-      <div className='overflow-hidden h-full sm:h-fit md:h-full  w-full sm:w-full sm:min-w-0 sm:min-h-0 md:w-full lg:min-h-[81.9vh] sm:flex sm:flex-col justify-around  md:grid  grid-rows-[auto_auto_auto_auto_100px] 2xl:grid-rows-[auto_auto_auto_auto_auto_2000px]' >
+      <div className='overflow-hidden h-full sm:h-fit md:h-full  w-full sm:w-full sm:min-w-0 sm:min-h-0 md:w-full lg:min-h-[81.9vh] sm:flex sm:flex-col justify-around  md:grid  grid-rows-[auto_auto_auto]' >
         <AutoCompleteAddress />
         <Cars />
         <Paymentcard />
