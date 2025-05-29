ccb802b3971e86932212d5ab82ef98cd1f792dfa
hexelency <hexelency@gmail.com>
2025-05-29 09:37:51 +0100

    all layout again am

--- app/(dashboard)/_components/map/MapSection.tsx
+++ app/(dashboard)/_components/map/MapSection.tsx
@@ -6,7 +6,7 @@ import React from 'react'
 const MapSection = () => {
   return (
     <div className='p-[0px] md:p-5 items-center justify-center align-middle flex flex-col'>
-      <h2 className='font-semibold text-[10px] md:text-2xl mt-0' >Map Guide</h2>
+      <h2 className='font-semibold text-[10px] md:text-2xl mt-0' >I Love Goodness Atube nothing wey person wan tell me </h2>
       {/* <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d301837.4338830072!2d6.497383468385589!3d6.284227668623471!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2sng!4v1748232359212!5m2!1sen!2sng" width="600" height="450" className='border-0 w-full'  loading="lazy" >
       mapoo</iframe> */}
       <div className="rounded-lg overflow-hidden">
