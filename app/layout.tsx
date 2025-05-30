8c46560e8c380d776bbce84b91e56d09f30efcc4
hexelency <hexelency@gmail.com>
2025-05-30 23:07:56 +0100

    og image addedss

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,7 +9,6 @@ import {
 } from '@clerk/nextjs'
 import { Geist, Geist_Mono, Outfit } from 'next/font/google'
 import './globals.css'
-import Head from 'next/head';
 // import NavBar from '@/app/(landingpage)/_components/NavBar'
 
 // const geistSans = Geist({
@@ -22,42 +21,34 @@ import Head from 'next/head';
 //   subsets: ['latin'],
 // })
 
-export const metadata = {
-  title: "Brokacars Dashboard",
-  description: "Comfortable rides with Brokacars.",
-  openGraph: {
+export const generateMetadata = (): Metadata => {
+  return {
     title: "Brokacars Dashboard",
     description: "Best Prices and Comfortable rides with Brokacars.",
-    url: "https://brokacars.vercel.app/dashboard",
-    siteName: "Brokacars",
-    images: [
-      {
-        url: "https://brokacars.vercel.app/og-image.png",
-        width: 1200,
-        height: 630,
-        alt: "Brokacars dashboard preview",
-      },
-    ],
-    locale: "en_US",
-    type: "website",
-  },
-  twitter: {
-    card: "summary_large_image",
-    title: "Brokacars Dashboard",
-    description: "Comfortable rides with Brokacars.",
-    images: ["https://brokacars.vercel.app/og-image.png"],
-  },
-};
-
-<Head>
-  <title>Brokacars Dashboard</title>
-  <meta property="og:title" content="Brokacars Dashboard" />
-  <meta property="og:description" content="Comfortable drives with Brokacars." />
-  <meta property="og:image" content="https://brokacars.vercel.app/og-image.jpg" />
-  <meta property="og:url" content="https://brokacars.vercel.app/dashboard" />
-  <meta name="twitter:card" content="summary_large_image" />
-</Head>
-
+    openGraph: {
+      title: "Brokacars Dashboard",
+      description: "Best Prices and Comfortable rides with Brokacars.",
+      url: "https://brokacars.vercel.app/dashboard",
+      siteName: "Brokacars",
+      images: [
+        {
+          url: "https://brokacars.vercel.app/og-image.png",
+          width: 1200,
+          height: 630,
+          alt: "Brokacars dashboard preview",
+        },
+      ],
+      locale: "en_US",
+      type: "website",
+    },
+    twitter: {
+      card: "summary_large_image",
+      title: "Brokacars Dashboard",
+      description: "Comfortable rides with Brokacars.",
+      images: ["https://brokacars.vercel.app/og-image.png"],
+    },
+  }
+}
 export default function RootLayout({
   children,
 }: Readonly<{
