fb6d5c236a0b6e5f4a908519a3e66632af5ff9b0
hexelency <hexelency@gmail.com>
2025-05-30 23:40:02 +0100

    og image meta2

--- app/(landingpage)/services/page.tsx
+++ app/(landingpage)/services/page.tsx
@@ -1,4 +1,33 @@
-import React from 'react'
+import { Metadata } from "next";
+
+export const generateMetadata = (): Metadata => {
+  return {
+    title: "Brokacars services",
+    description: "Best Prices and Comfortable rides with Brokacars",
+    openGraph: {
+      title: "Brokacars Homepage",
+      description: "Best Prices and Comfortable rides with Brokacars.",
+      url: "https://brokacars.vercel.app/services",
+      siteName: "Brokacars service page",
+      images: [
+        {
+          url: "https://brokacars.vercel.app/og-image.jpg",
+          width: 1200,
+          height: 630,
+          alt: "Brokacars services preview",
+        },
+      ],
+      locale: "en_US",
+      type: "website",
+    },
+    twitter: {
+      card: "summary_large_image",
+      title: "Brokacars homepage",
+      description: "Comfortable rides with Brokacars.",
+      images: ["https://brokacars.vercel.app/og-image.jpg"],
+    },
+  }
+}
 
 const page = () => {
   return (
