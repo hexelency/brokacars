0ccd27631886f23e0811225bc6a8ed4add758adf
hexelency <hexelency@gmail.com>
2025-05-30 23:25:35 +0100

    og image added meta

--- app/(landingpage)/page.tsx
+++ app/(landingpage)/page.tsx
@@ -1,6 +1,37 @@
 // import { UserButton } from "@clerk/nextjs";
 // import Image from "next/image";
 
+import { Metadata } from "next";
+
+export const generateMetadata = (): Metadata => {
+  return {
+    title: "Brokacars homepage",
+    description: "Best Prices and Comfortable rides with Brokacars",
+    openGraph: {
+      title: "Brokacars Homepage",
+      description: "Best Prices and Comfortable rides with Brokacars.",
+      url: "https://brokacars.vercel.app/",
+      siteName: "Brokacars",
+      images: [
+        {
+          url: "https://brokacars.vercel.app/og-image.png",
+          width: 1200,
+          height: 630,
+          alt: "Brokacars dashboard preview",
+        },
+      ],
+      locale: "en_US",
+      type: "website",
+    },
+    twitter: {
+      card: "summary_large_image",
+      title: "Brokacars homepage",
+      description: "Comfortable rides with Brokacars.",
+      images: ["https://brokacars.vercel.app/og-image.png"],
+    },
+  }
+}
+
 export default function Home() {
   return (
     <>
